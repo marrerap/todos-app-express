0f5f849c517d67b68028a1454fd7f8ba15de6718
marrerap <andrew.marrero@outlook.com>
2021-07-21 16:02:30 -0500

    added the toggle complete.js

--- public/script.js
+++ public/script.js
@@ -1,47 +1,92 @@
+// const { json } = require("express")
+
 function renderTodos(todosArray) {
     const todosHtmlArray = todosArray.map(todo => {
-        return `<li class="${todo.completed ? 'completed' : 'incomplete'}"> ${todo.text}</li>`
+        return `<li class="${todo.completed ? 'completed' : 'incomplete'}"> ${todo.text}</li><br>
+        <button class="delete-button" data-id="${todo.id}">Delete</button>
+        <button class="toggle-complete" data-id="${todo.id}" >Completed?</button>`
     })
     return todosHtmlArray.join('')
 }
 
 
 function fetchTodos() {
-   fetch('/api/v1/todos')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        todos.innerHTML = renderTodos(data)
-}) 
+    fetch('/api/v1/todos')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            todos.innerHTML = renderTodos(data)
+        })
 }
 
 const todos = document.getElementById('todos')
 const todoForm = document.getElementById('todoForm')
 
 
-fetchTodos()
+fetchTodos() 
 
 
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault()
-        const todoText = document.getElementById('todo_text')
-        fetch('/api/v1/todos', {
-            method: 'POST',
+    const todoText = document.getElementById('todo_text')
+    fetch('/api/v1/todos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: json.stringify({
+            text: todoText.value
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            fetchTodos()
+            todoForm.reset
+        })
+})
+
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete-button')) {
+        const id = e.target.dataset.id
+        fetch(`/api/v1/todos/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => !res.ok &&
+                res.json())
+            .then(data => {
+                if (data.error) {
+                    alert(data.error)
+                }
+                fetchTodos()
+
+            })
+
+
+    }
+})
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('toggle-complete')) {
+        const id = e.target.dataset.id
+        fetch(`/api/v1/todos/${id}`, {
+            method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                text: todoText.value
+                completed: true
             })
+            
         })
-    .then(res => res.json())
-    .then(data => {
-        fetchTodos()
-        todoForm.reset
-    })
+            .then(res => !res.ok &&
+                res.json())
+            .then(data => {
+                if (data.error) {
+                    alert(data.error)
+                }
+                fetchTodos()
+
+            })
+
+
+    }
 })
-
-
-
-
-
