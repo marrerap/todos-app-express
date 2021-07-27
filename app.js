b5313c4ff65ee8cd5ed3dd680b77ef26a43e9a51
marrerap <andrew.marrero@outlook.com>
2021-07-26 20:22:01 -0500

    add pg-promise and config file

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const http = require('http');
 const express = require("express")
-const db = require("./model/db.js")
+const pgp = require('pg-promise')();
+const config = require('./config');
+const db = pgp(config);
+
+
+
 let id = 6
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const app = express();
 
+
+const app = express();
 const server = http.createServer(app)
+
+
 app.use(express.static('./public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'todos',
+    user: 'postgres',
+    password: '',
+
+
+}
+
+module.exports = config
